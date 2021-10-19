065eb4e1694973adda135e8205546f26bc576f3e
John Mataya <johnmataya@gmail.com>
2021-10-19 11:41:49 -0400

    Update libraries.R

--- functions/libraries.R
+++ functions/libraries.R
@@ -1,1 +1,39 @@
-library(tidyverse)
+#install.packages("ggthemes")
+#install.packages("modeltime")
+
+
+#shiny libraries
+suppressMessages(library(shiny))
+suppressMessages(library(shinydashboard))
+suppressMessages(library(shinycssloaders))
+suppressMessages(library(shinythemes))
+suppressMessages(library(DT))
+
+
+
+#suppressMessages(library(shinydashboardPlus))
+#suppressMessages(library(shinyWidgets))
+#suppressMessages(library(shinyalert))
+#suppressMessages(library(shinyjs))
+#suppressMessages(library(shinyBS))
+
+## model 
+suppressMessages(library(ranger))
+library(modeltime)
+
+
+#tidyverse and other libraries
+suppressMessages(library(tidyverse))
+suppressMessages(library(plotly))
+suppressMessages(library(heatmaply))
+suppressMessages(library(ggthemes))
+suppressMessages(library(ggjoy))
+#suppressMessages(library(d3heatmap))
+suppressMessages(library(leaflet))
+suppressMessages(library(RColorBrewer))
+suppressMessages(library(lubridate))
+suppressMessages(library(janitor))
+suppressMessages(library(timetk))
+
+
+
