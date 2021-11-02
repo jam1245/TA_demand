aacd79f822f46978bff9bbb77e58aee6fb01d0cd
John Mataya <johnmataya@gmail.com>
2021-11-02 15:52:23 -0400

    update libs

--- library/library.R
+++ library/library.R
@@ -1,4 +1,33 @@
 
-library(tidyverse)
-library(tidyr)
-library(leaflet)
+#shiny libraries
+suppressMessages(library(shiny))
+suppressMessages(library(shinydashboard))
+suppressMessages(library(shinycssloaders))
+suppressMessages(library(shinythemes))
+suppressMessages(library(DT))
+
+
+
+#suppressMessages(library(shinydashboardPlus))
+#suppressMessages(library(shinyWidgets))
+#suppressMessages(library(shinyalert))
+#suppressMessages(library(shinyjs))
+#suppressMessages(library(shinyBS))
+
+## model 
+suppressMessages(library(ranger))
+library(modeltime)
+
+
+#tidyverse and other libraries
+suppressMessages(library(tidyverse))
+suppressMessages(library(plotly))
+suppressMessages(library(heatmaply))
+suppressMessages(library(ggthemes))
+suppressMessages(library(ggjoy))
+#suppressMessages(library(d3heatmap))
+suppressMessages(library(leaflet))
+suppressMessages(library(RColorBrewer))
+suppressMessages(library(lubridate))
+suppressMessages(library(janitor))
+suppressMessages(library(timetk))
